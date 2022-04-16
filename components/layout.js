7deed4b31e4e14ab241d25c6beb8ea28f9da3a90
Francesco <fduffyboscagli@gmail.com>
2022-04-16 14:30:55 -0700

    :lipstick: adding initial layout with tailwindcss

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,12 @@
 export default function Layout({ children }) {
-  return <div>Hello!{children}</div>
+  return (
+    <div className="bg-monstera-800 text-white">
+      <header className="pt-[50px] mx-[24px]">
+        <nav id="explore" className="bg-sun-400 flex flex-row justify-between items-center h-[40px] w-full">
+          <div id="profile-pic" className="bg-monstera-100 w-[36px] h-[36px]"></div>
+        </nav>
+        <div id="title-area"></div>
+      </header>
+    </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,26 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#FCFEF8',
+      'purple': '#3f3cbb',
+      'grey': {
+        100: '#ECECEC',
+        600: '#606F73',
+      },
+      'sun': {
+        100: '#FFF3B7',
+        400: "#FAD042"
+      },
+      'monstera': {
+        200: '#D7FFB7',
+        300: '#8DB572',
+        400: '#5C8B57',
+        800: '#3F5214',
+      },
+    },
     extend: {},
   },
   plugins: [],
