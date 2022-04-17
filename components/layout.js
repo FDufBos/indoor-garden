7538b871e637a84ba3aa35bd6173622e370a8515
Francesco <fduffyboscagli@gmail.com>
2022-04-16 18:08:08 -0700

    :sparkle: Basic layout

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,11 @@
 import Image from 'next/image'
-import profilePic from '../public/images/memoji/male-1.png'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+const downArrow = <FontAwesomeIcon icon={faAngleDown} />
+
+import styles from '../styles/Home.module.css'
+
 
 export default function Layout({ children }) {
   return (
@@ -7,14 +13,32 @@ export default function Layout({ children }) {
       <header className="flex flex-col gap-4 mt-4 mx-6">
         <nav id="explore" className=" flex flex-row justify-between items-center h-[40px] w-full">
           <div id="profile-pic" className="bg-monstera-200 drop-shadow-sm w-[40px] h-[40px] flex justify-center items-center rounded-full">
-            <Image src={profilePic} width="35" height="35" className='drop-shadow'/>
+            <Image src="/images/memoji/male-1.png" width="30" height="30" className='drop-shadow'/>
           </div>
-          <div id="codex-button" className='bg-white drop-shadow-sm rounded-full text-grey-600 px-[16px] py-[8px] text-center hover:scale-110 transition-all'>🌱 Exchange</div>
+          <div id="codex-button" className='bg-white drop-shadow-sm rounded-full text-grey-600 px-4 py-2 text-center hover:scale-110 transition-all'>🌱 Exchange</div>
         </nav>
-        <div id="title-area flex flex-row justify-between items-center h-[40px] w-full">
-          <h1 className='font-alpina'>Indoor Garden</h1>
+        <div id="title-area" className="flex flex-row justify-between items-end w-full">
+          <div className="flex flex-row items-baseline gap-2">
+            <h1 className='font-alpina'>Indoor Garden</h1>
+            <div>{downArrow}</div>
+          </div>
+          <Image src="/images/sun.svg" width="40" height="40"/>
         </div>
+        <div id="line" className='w-full h-[1px] bg-white opacity-75 -translate-y-1'></div>
       </header>
+      {children}
+      <footer className={styles.footer}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <span className={styles.logo}>
+              <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            </span>
+          </a>
+        </footer>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,34 +6,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      slate: colors.slate,
-      gray: colors.gray,
-      red: colors.red,
-      'white': '#FCFEF8',
-      'purple': '#3f3cbb',
-      'grey': {
-        100: '#ECECEC',
-        600: '#606F73',
-      },
-      'sun': {
-        100: '#FFF3B7',
-        400: "#FAD042"
-      },
-      'monstera': {
-        200: '#D7FFB7',
-        300: '#8DB572',
-        400: '#5C8B57',
-        800: '#3F5214',
-      },
-    },
     extend: {
       fontFamily: {
         flexa: ["Flexa", "sans-serif"],
         alpina: ["Alpina", "serif"],
+      },
+
+      colors: {
+        'white': '#FCFEF8',
+        'purple': '#3f3cbb',
+        'grey': {
+          100: '#ECECEC',
+          600: '#606F73',
+        },
+        'sun': {
+          100: '#FFF3B7',
+          400: "#FAD042"
+        },
+        'monstera': {
+          200: '#D7FFB7',
+          300: '#8DB572',
+          400: '#5C8B57',
+          800: '#3F5214',
+        },
       },
     },
   },
