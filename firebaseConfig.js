b531d1ad8b877ea677aa47d17e095a3717f60191
Francesco <fduffyboscagli@gmail.com>
2022-04-17 13:48:08 -0700

    :package: Firebase added

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKv_lZfpfFkdjtqJptVwv-talUnYrMUbo",
+  authDomain: "indoor-garden-b4585.firebaseapp.com",
+  projectId: "indoor-garden-b4585",
+  storageBucket: "indoor-garden-b4585.appspot.com",
+  messagingSenderId: "1093253453203",
+  appId: "1:1093253453203:web:d34b9fe7e402b010d9c466"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
