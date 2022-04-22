d655beeecbdc6b041ba00a00c8f10e9e8c515cdf
Francesco <fduffyboscagli@gmail.com>
2022-04-21 17:10:51 -0700

    :lipstick: • Lightened bg color • Tighetned gap between water & fire labels

--- components/plantItem.js
+++ components/plantItem.js
@@ -5,11 +5,11 @@ export default function PlantItem({children, icon, name, popularName,timeTillNex
         <div id="image-circle" className="bg-white w-16 h-16 rounded-full drop-shadow flex flex-col justify-center items-center">
           <div id='plant-image' className="text-4xl text-center">{icon}</div>
         </div>
-        <div id="image-label" className="flex items-center justify-center bg-sun-400 h-6 w-6 rounded-full drop-shadow text-grey-600 font-[690] text-sm relative top-11 -left-6">{level}</div>
+        <div id="image-label" className="flex items-center justify-center bg-water-100 h-6 w-6 rounded-full drop-shadow text-grey-600 font-[690] text-sm relative top-11 -left-6">{level}</div>
         <div className="flex flex-col h-full">
           <h3 className="text-xl font-[580]">{name}</h3>
           <h4 className="text-xs text-water-100">{popularName}</h4>
-          <div className="flex gap-4">
+          <div className="flex gap-2">
             <div>💧 {timeTillNextWater}</div><div>🔥 {wateringStreak}</div>
           </div>
         </div>
