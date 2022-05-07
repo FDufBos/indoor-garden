740dcc05d2829ffb456001ab02131f918e60fd60
Francesco <fduffyboscagli@gmail.com>
2022-05-07 11:51:03 -0700

    Deleted a bunch of unused code

--- components/atoms/basicButton.js
+++ components/atoms/basicButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function BasicButton({
   onPress,
@@ -11,20 +11,11 @@ export default function BasicButton({
   otherText,
   icon,
 }) {
-  const [pressState, setPressState] = useState(false);
-
-  const whenClicked = () => {
-    setPressState((prevState) => !prevState)
-  }
-
   return (
-    <button
-      onClick={() => whenClicked(pressState)}
+    <div
       className={` ${textColor} ${bgColor} text-gray-600 min-w-fit flex flex-row items-center ${xPadding} ${yPadding} px-[10px] py-[5px] rounded-full gap-2 shadow-md transition-all duration-500 text-sm	`}
     >
-      {pressState ? innerText : otherText}
-      <div className={`${pressState ? "-rotate-180" : "-rotate-90"} transition-all origin-[50%_45%] duration-500	`}>{icon}</div>
       {children}
-    </button>
+    </div>
   );
 }
