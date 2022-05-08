305805c8176c5aea65408fcb88903ef468dbedb5
Francesco <fduffyboscagli@gmail.com>
2022-05-08 16:36:39 -0700

    :bulb: comment out

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,33 +1,33 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+// import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDKv_lZfpfFkdjtqJptVwv-talUnYrMUbo",
-  authDomain: "indoor-garden-b4585.firebaseapp.com",
-  projectId: "indoor-garden-b4585",
-  storageBucket: "indoor-garden-b4585.appspot.com",
-  messagingSenderId: "1093253453203",
-  appId: "1:1093253453203:web:d34b9fe7e402b010d9c466"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDKv_lZfpfFkdjtqJptVwv-talUnYrMUbo",
+//   authDomain: "indoor-garden-b4585.firebaseapp.com",
+//   projectId: "indoor-garden-b4585",
+//   storageBucket: "indoor-garden-b4585.appspot.com",
+//   messagingSenderId: "1093253453203",
+//   appId: "1:1093253453203:web:d34b9fe7e402b010d9c466"
+// };
 
 
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
+// // Initialize Cloud Firestore and get a reference to the service
+// const db = getFirestore(app);
 
-//collection ref
-const colRef = collection(db, 'plants')
+// //collection ref
+// const colRef = collection(db, 'plants')
 
-//get collection data
-getDocs(colRef)
-  .then((snapshot)=> {
-    console.log(snapshot.docs)
-  })
+// //get collection data
+// getDocs(colRef)
+//   .then((snapshot)=> {
+//     console.log(snapshot.docs)
+//   })
