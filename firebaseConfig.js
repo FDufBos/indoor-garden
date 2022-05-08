3edbbf92d079d5037200ca6a354e2d08478fc36e
Francesco <fduffyboscagli@gmail.com>
2022-05-08 14:45:05 -0700

    :wrench: Added firestore to config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,5 +15,19 @@ const firebaseConfig = {
   appId: "1:1093253453203:web:d34b9fe7e402b010d9c466"
 };
 
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+//collection ref
+const colRef = collection(db, 'plants')
+
+//get collection data
+getDocs(colRef)
+  .then((snapshot)=> {
+    console.log(snapshot.docs)
+  })
