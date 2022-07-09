57032e1f5cdbda9ddd937b157745350e4adb5717
Francesco <fduffyboscagli@gmail.com>
2022-07-09 16:24:20 -0700

    :wastebasket: remove font awesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    
       <Component {...pageProps} />
-    
-  )
+
+  );
 }
 
-export default MyApp
+export default MyApp;
