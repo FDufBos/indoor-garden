0575b765d78daac76b88e763a65e78ed335567e7
Francesco <fduffyboscagli@gmail.com>
2022-07-09 16:23:44 -0700

    :wastebasket: cleaned up button props

--- components/atoms/basicButton.js
+++ components/atoms/basicButton.js
@@ -1,19 +1,13 @@
 import React from "react";
 
 export default function BasicButton({
-  onPress,
   children,
   bgColor,
   textColor,
-  xPadding,
-  yPadding,
-  innerText,
-  otherText,
-  icon,
 }) {
   return (
     <div
-      className={` ${textColor} ${bgColor} text-gray-600 min-w-fit flex flex-row items-center ${xPadding} ${yPadding} px-[10px] py-[5px] rounded-full gap-2 shadow-md transition-all duration-500 text-sm	`}
+      className={` ${textColor} ${bgColor} text-gray-600 min-w-fit flex flex-row items-center px-[10px] py-[5px] rounded-full gap-2 shadow-md transition-all duration-500 text-sm	`}
     >
       {children}
     </div>
