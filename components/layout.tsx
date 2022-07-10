8a5a7568c9488fb155b5fb66882073135a45e9b6
Francesco <fduffyboscagli@gmail.com>
2022-07-10 00:50:35 -0700

    :wastebasket: removed add plant button from nav

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,22 @@
 import Image from "next/image";
+import React from "react";
+import { render } from "react-dom";
+import { useState } from "react";
 
-export default function Layout({ children }) {
+import NewForm from "./NewForm";
+
+export default function Layout({ children}) {
+
+  const [newPlantModalOpen, setnewPlantModalOpen] = useState(false);
+  const plantList: object[] = [];
+  const [plants, setPlants] = useState(plantList);
+
+  const handleClick = () => {
+    //when the user clicks the button, set the state of newPlantModalOpen to true
+    setnewPlantModalOpen(true);
+    //set the state of newPlantModalOpen to false after the modal is closed
+  };
+  
   return (
     <div className=" text-white">
       <header className="flex flex-col gap-4 mt-4 mx-6">
@@ -20,20 +36,13 @@ export default function Layout({ children }) {
             />
           </div>
           <div className="flex flex-row gap-4">
-          <div
-            id="exchange-button"
-            className="bg-white drop-shadow-sm rounded-full text-grey-600 px-4 py-2 text-center font-[558] hover:scale-110 transition-all"
-          >
-            🌱 Exchange
-          </div>
-          <div
-            id="exchange-button"
-            className="bg-white drop-shadow-sm rounded-full text-grey-600 px-4 py-2 text-center font-[558] hover:scale-110 transition-all"
-          >
-             ➕ New Plant
-          </div>
-          </div>
-          
+            <div
+              id="exchange-button"
+              className="bg-white drop-shadow-sm rounded-full text-grey-600 px-4 py-2 text-center font-[558] hover:scale-110 transition-all"
+            >
+              🌱 Exchange
+            </div>
+            </div>
         </nav>
         <div
           id="title-area"
