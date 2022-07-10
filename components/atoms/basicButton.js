da6975450a36825765b5cd32308e6d9c4580f262
Francesco <fduffyboscagli@gmail.com>
2022-07-10 15:26:26 -0700

    :art: :lipstick: • Improved structure
    • Added hover

--- components/atoms/basicButton.js
+++ components/atoms/basicButton.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-export default function BasicButton({
-  children,
-  bgColor,
-  textColor,
-}) {
+export default function BasicButton({ children, bgColor, textColor }) {
   return (
     <div
-      className={` ${textColor} ${bgColor} text-gray-600 min-w-fit flex flex-row items-center px-[10px] py-[5px] rounded-full gap-2 shadow-md transition-all duration-500 text-sm	`}
+      className={` 
+      ${textColor} 
+      ${bgColor}
+       text-gray-600 text-sm
+       min-w-fit 
+       flex flex-row gap-2 items-center 
+       px-[10px] py-[5px] 
+       rounded-full shadow-lg hover:shadow-sm 
+       hover:scale-95
+       transition-all duration-100 ease-in
+       	`}
     >
       {children}
     </div>
