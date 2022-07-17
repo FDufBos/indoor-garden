8e6ff85c753501f5f2941d04680f4359198ec038
Francesco <fduffyboscagli@gmail.com>
2022-07-17 01:49:31 -0700

    :sparkles: added delete for firestore
    :sparkles: added a bunch of functions for auth

--- data/firestore.ts
+++ data/firestore.ts
@@ -2,7 +2,15 @@
 import { initializeApp } from "firebase/app";
 // Firebase SDKs Here
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore, collection, getDocs, getDoc, doc, addDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -23,12 +31,11 @@ export const db = getFirestore(app);
 //collection ref
 const colRef = collection(db, "plants");
 
-
 //CREATE
 
 export const addPlant = async (plant) => {
-  await addDoc((colRef), plant);
-}
+  await addDoc(colRef, plant);
+};
 
 //READ
 
@@ -42,13 +49,78 @@ export const fetchIDs = async () => {
   const plantsSanpshot = await getDocs(collection(db, "plants"));
   const idList = plantsSanpshot.docs.map((doc) => doc.id);
   return idList;
-}
+};
 
 export const fetchPlant = async (nickname) => {
-  const plantSnapshot = await getDoc(doc(db, 'plants', nickname));
+  const plantSnapshot = await getDoc(doc(db, "plants", nickname));
   if (plantSnapshot.exists()) {
-      return plantSnapshot.data();
+    return plantSnapshot.data();
   } else {
-      console.log("Plant doesn't exist");
+    console.log("Plant doesn't exist");
   }
+};
+
+//DELETE
+export const deletePlant = async (plant) => {
+  await deleteDoc(doc(db, "plants", plant));
+};
+
+//🔒🔒🔒🔒🔒🔒🔒 AUTH 🔒🔒🔒🔒🔒🔒🔒🔒🔒
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+const auth = getAuth();
+
+//👶 Create a new user with email and password
+
+export const createUser = async (email: string, password) => {
+  await createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      //console log the user's email address
+    })
+    .then(async () => {
+      await sendEmailVerification(auth.currentUser);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+};
+
+//sign in with email and password
+export const signIn = async (email: string, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
+
+
+
+//👋 Sign out
+export const logOut = async () => {
+  signOut(auth)
+    .then(() => {
+      console.log("Signed out");
+    })
+    .catch((error) => {
+      // An error happened.
+    });
 };
