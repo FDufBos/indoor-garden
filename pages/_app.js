b3ce104cf5dd52982b5506caaed5abf55a303d7a
Francesco <fduffyboscagli@gmail.com>
2022-07-17 01:49:53 -0700

    :wrench: Added in Chakra
    :lipstick: included themes and fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import "../styles/globals.css";
-
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from "../styles/theme";
+import Fonts from '../styles/Fonts';
 
 
 
 
 function MyApp({ Component, pageProps }) {
   return (
-
+    <ChakraProvider theme={theme}>
+      <Fonts />
       <Component {...pageProps} />
+    </ChakraProvider>
+      
 
 
   );
