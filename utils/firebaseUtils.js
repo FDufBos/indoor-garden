a2fbb8d3254c697e8e57cbc1ca7c7ecbe6467277
Francesco <fduffyboscagli@gmail.com>
2022-07-18 22:47:43 -0700

    :fire: Removed all unnecessary Auth functions (now found in AuthContext.js)
    * Added getUserNameFromFirebase, but will probably also remove soon

---
+++ utils/firebaseUtils.js
@@ -0,0 +1,25 @@
+
+
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDKv_lZfpfFkdjtqJptVwv-talUnYrMUbo",
+  authDomain: "indoor-garden-b4585.firebaseapp.com",
+  projectId: "indoor-garden-b4585",
+  storageBucket: "indoor-garden-b4585.appspot.com",
+  messagingSenderId: "1093253453203",
+  appId: "1:1093253453203:web:d34b9fe7e402b010d9c466",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
+
+export default app
