df7f2e8f743299167fe6302194f53bf7bad7f714
Francesco <fduffyboscagli@gmail.com>
2022-07-18 22:50:40 -0700

    :fire: removed all auth and moved to AuthContext

--- data/firestore.ts
+++ data/firestore.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {useState} from "react";
 // Firebase SDKs Here
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {
@@ -27,7 +28,7 @@ import {
 } from "firebase/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDKv_lZfpfFkdjtqJptVwv-talUnYrMUbo",
   authDomain: "indoor-garden-b4585.firebaseapp.com",
   projectId: "indoor-garden-b4585",
@@ -37,7 +38,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
@@ -76,68 +77,18 @@ export const fetchPlant = async (nickname, user) => {
   }
 };
 
+export const getUserNameFromFirebase = async (user) => {
+  const userSnapshot = await getDoc(doc(db, "users", user));
+  if (userSnapshot.exists()) {
+    return userSnapshot.data().name
+  } else {
+    console.log("User doesn't exist || This is being called from getUserNameFromFirebase");
+  }
+}
+
 //DELETE PLANT
 export const deletePlant = async (plant, user) => {
   console.log(plant)
   console.log(user)
   await deleteDoc(doc(db, `users/${user}/garden`, plant));
-};
-
-//🔒🔒🔒🔒🔒🔒🔒 AUTH 🔒🔒🔒🔒🔒🔒🔒🔒🔒
-
-//👶 CREATE NEW USER 👶
-
-export const createUser = async (
-  email: string,
-  password: string,
-  name: string
-) => {
-  await createUserWithEmailAndPassword(auth, email, password)
-    .then(async (userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      //update display name
-
-      await setDoc(doc(db, "users", user.uid), {
-        uid: user.uid,
-        authProvider: "Username and Password",
-        email,
-        name: name,
-        timeCreated: serverTimestamp(),
-      });
-    })
-    .then(async () => {
-      await sendEmailVerification(auth.currentUser);
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage);
-      // ..
-    });
-};
-
-//🧑 SIGN IN WITH EMAIL AND PASSWORD 🧑
-export const signIn = async (email: string, password: string) => {
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      console.log(user);
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage);
-    });
-};
-
-//🚪 SIGN OUT 🚪
-export const logOut = async () => {
-  signOut(auth)
-    .then(() => {})
-    .catch((error) => {
-      // An error happened.
-    });
 };
