45319119a813f3d12cf47f3ce7995df3c5497dc4
Francesco <fduffyboscagli@gmail.com>
2022-07-20 16:54:11 -0700

    :lipstick: :wheelchair: Replacing elements with chakra ui

--- components/newForm.tsx
+++ components/newForm.tsx
@@ -14,6 +14,11 @@ import {
   ModalBody,
   ModalCloseButton,
   useDisclosure,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
 } from "@chakra-ui/react";
 
 import { getRandomEmoji } from "../data/randomEmoji";
@@ -25,7 +30,7 @@ export default function NewForm({
   isOpen,
   onClose,
   setFirestorePlants,
-  firestorePlants
+  firestorePlants,
 }) {
   //state for emoji
   const [emoji, setEmoji] = useState("🌱");
@@ -35,16 +40,15 @@ export default function NewForm({
     const iv = setInterval(() => {
       setEmoji(getRandomEmoji("Animals & Nature", "plant-flower"));
     }, 90);
-
     return setTimeout(() => {
       clearInterval(iv);
     }, 1800);
-    // setEmoji(getRandomEmoji('Animals & Nature'));
   };
 
   //handle emoji change
   const handleEmojiChange = (e) => {
     //animate when value updates
+    e.preventDefault();
     setEmoji(e.target.value);
   };
 
@@ -66,7 +70,7 @@ export default function NewForm({
     );
     const icon = e.currentTarget.elements.icon.value;
 
-    const timestamp=serverTimestamp();
+    const timestamp = serverTimestamp();
     // console.log(timestamp);
 
     addPlant(
@@ -86,37 +90,35 @@ export default function NewForm({
     //reset and close form
     // e.currentTarget.reset();
     onClose();
-    setFirestorePlants(firestorePlants.concat([{
-      icon: icon,
-      commonName: commonName,
-      nickname: nickname,
-      timeTillNextWater: 0,
-      wateringStreak: 0,
-      level: 1,
-      timeCreated: serverTimestamp(),
-      // id: nickname,
-    }]));
-
+    setFirestorePlants(
+      firestorePlants.concat([
+        {
+          icon: icon,
+          commonName: commonName,
+          nickname: nickname,
+          timeTillNextWater: 0,
+          wateringStreak: 0,
+          level: 1,
+          timeCreated: serverTimestamp(),
+          // id: nickname,
+        },
+      ])
+    );
   };
 
   const handleClose = () => {
     onClose();
   };
 
- 
   return (
     <div>
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay backdropFilter="blur(20px)" bg="blackAlpha.100" />
-        <ModalContent
-          bg="#e7f9ec"
-          borderRadius="2xl"
-          p={8}
-        >
+        <ModalContent bg="#e7f9ec" borderRadius="xl" p={4}>
           <ModalCloseButton />
           <form onSubmit={handleSubmit}>
             <ModalBody display="flex" flexDirection="column" gap="16px">
-              <div className="flex flex-col justify-center items-center gap-1">
+              <FormControl className="flex flex-col justify-center items-center gap-1">
                 <div className="flex flex-row gap-4" onClick={handleEmojiClick}>
                   <motion.button
                     id="emoji-button"
@@ -130,7 +132,7 @@ export default function NewForm({
                       disabled
                       required
                       maxLength={1}
-                      type="text"
+                      type="button"
                       name="icon"
                       className="text-4xl w-16 h-16 text-center shrink rounded-full  shadow-lg bg-white placeholder-monstera-400 placeholder-opacity-60 border-[1px] border-[#ccebd7] text-green-900 outline-[#5bb98c] outline-2 focus:outline focus:border-[#e7f9ec] cursor-pointer pointer-events-none"
                       value={emoji}
@@ -138,31 +140,37 @@ export default function NewForm({
                     />
                   </motion.button>
                 </div>
-              </div>
-              <div className="flex flex-col gap-1 ">
-                <label htmlFor="nickname" className="text-sm text-green-900">
+              </FormControl>
+              <FormControl className="flex flex-col gap-1 ">
+                <FormLabel
+                  htmlFor="nickname"
+                  className="text-sm text-green-900"
+                >
                   Nickname:
-                </label>
-                <input
+                </FormLabel>
+                <Input
                   placeholder="Mike"
                   required
                   type="text"
                   name="nickname"
                   className="rounded-md px-2 py-1 bg-[#e7f9ec] placeholder-monstera-400 placeholder-opacity-60 border-[1px] border-[#ccebd7] text-green-900 outline-[#5bb98c] outline-2 focus:outline focus:border-[#e7f9ec]"
                 />
-              </div>
-              <div className="flex flex-col gap-1">
-                <label htmlFor="commonName" className="text-sm text-green-900">
+              </FormControl>
+              <FormControl className="flex flex-col gap-1">
+                <FormLabel
+                  htmlFor="commonName"
+                  className="text-sm text-green-900"
+                >
                   Type of Plant:
-                </label>
-                <input
+                </FormLabel>
+                <Input
                   placeholder="e.g. Monstera"
                   required
                   type="text"
                   name="commonName"
                   className="rounded-md px-2 py-1 bg-[#e7f9ec] placeholder-monstera-400 placeholder-opacity-60 border-[1px] border-[#ccebd7] text-green-900 outline-[#5bb98c] outline-2 focus:outline focus:border-[#e7f9ec]"
                 />
-              </div>
+              </FormControl>
             </ModalBody>
             <ModalFooter
               display="flex"
