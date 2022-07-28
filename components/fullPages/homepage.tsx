322c05d7f30e4063d2b72d500e84bb6d2c20fbe0
Francesco <fduffyboscagli@gmail.com>
2022-07-28 00:16:23 -0700

    :dizzy: Page transitions

--- components/fullPages/homepage.tsx
+++ components/fullPages/homepage.tsx
@@ -15,13 +15,24 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useUserAuth } from "../../contexts/AuthContext";
 import { auth } from "../../utils/firebaseUtils";
 
+const variants = {
+  hidden: { x: "-20px", opacity: 0 },
+  enter: { x: "0px", opacity: 1 },
+  exit: { x: "-100px", opacity: 0 },
+};
+
 export default function Homepage() {
   // const [firestorePlants, setFirestorePlants] = useState([]);
   // const [documentIDs, setDocumentIDs] = useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { user, firestorePlants, documentIDs, setFirestorePlants, setDocumentIDs } = useUserAuth();
+  const {
+    user,
+    firestorePlants,
+    documentIDs,
+    setFirestorePlants,
+    setDocumentIDs,
+  } = useUserAuth();
   // const { user } = useUserAuth();
-
 
   useEffect(() => {
     // console.log("homepage.tsx firestorePlants in useEffect: " + firestorePlants)
@@ -41,81 +52,91 @@ export default function Homepage() {
     // });
   }, []);
 
-
-
   const handleNewFormClick = (e) => {
     e.preventDefault();
     onOpen();
   };
 
   return (
-    <Layout>
-      <div>
-        <Head>
-          <title>Indoor Garden</title>
-          <meta name="description" content="An Indoor Garden for Ya" />
-          <link rel="icon" href="/favicon.ico" />
+    <motion.div
+      variants={variants}
+      initial="hidden"
+      animate="enter"
+      exit="exit"
+      transition={{ type: "intertia"}}
+    >
+      <Layout>
+        <div>
+          <Head>
+            <title>Indoor Garden</title>
+            <meta name="description" content="An Indoor Garden for Ya" />
+            <link rel="icon" href="/favicon.ico" />
 
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#5C8B57" />
-          <link rel="apple-touch-icon" href="/images/app_icons/icon.png" />
-          <meta name="apple-mobile-web-app-status-bar" content="#5C8B57" />
-        </Head>
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="theme-color" content="#5C8B57" />
+            <link rel="apple-touch-icon" href="/images/app_icons/icon.png" />
+            <meta name="apple-mobile-web-app-status-bar" content="#5C8B57" />
+          </Head>
 
-        <main className="min-h-screen">
-          <section className=" mx-6">
-            {firestorePlants &&
-              firestorePlants.map((plant, index) => (
-                <Link href={`/garden/${documentIDs[index]}`} key={index} passHref>
-                  <div className="cursor-pointer">
-                    <PlantItem
-                      index={index}
-                      key={index}
-                      icon={plant.icon}
-                      name={plant.nickname}
-                      commonName={plant.commonName}
-                      timeTillNextWater={
-                        Math.floor(
-                          (Date.now() - plant.timeLastWatered.toDate()) /
-                            (1000 * 60 * 60 * 24)
-                        )
-                        // 5
-                      }
-                      wateringStreak={
-                        //calculate number of days since plant was created
-                        Math.floor(
-                          (Date.now() - plant.timeCreated.toDate()) /
-                            (1000 * 60 * 60 * 24)
-                        )
-                      }
-                      level={plant.level}
-                      timeCreated={plant.timeCreated}
-                    />
-                  </div>
-                </Link>
-              ))}
-          </section>
+          <main className="min-h-screen">
+            <section className=" mx-6">
+              {firestorePlants &&
+                firestorePlants.map((plant, index) => (
+                  <Link
+                    href={`/garden/${documentIDs[index]}`}
+                    key={index}
+                    passHref
+                  >
+                    <div className="cursor-pointer">
+                      <PlantItem
+                        index={index}
+                        key={index}
+                        icon={plant.icon}
+                        name={plant.nickname}
+                        commonName={plant.commonName}
+                        timeTillNextWater={
+                          Math.floor(
+                            (Date.now() - plant.timeLastWatered.toDate()) /
+                              (1000 * 60 * 60 * 24)
+                          )
+                          // 5
+                        }
+                        wateringStreak={
+                          //calculate number of days since plant was created
+                          Math.floor(
+                            (Date.now() - plant.timeCreated.toDate()) /
+                              (1000 * 60 * 60 * 24)
+                          )
+                        }
+                        level={plant.level}
+                        timeCreated={plant.timeCreated}
+                      />
+                    </div>
+                  </Link>
+                ))}
+            </section>
 
-          <div>
-            <NewForm
-              isOpen={isOpen}
-              onClose={onClose}
-              setFirestorePlants={setFirestorePlants}
-              setDocumentIDs={setDocumentIDs}
-            />
-          </div>
-
-          {user ? (
-            <Button onClick={handleNewFormClick} className="mx-6 mb-10">
-              New Plant
-            </Button>
-          ) : (
-            <div className="mx-6 mb-10">
-              <div>Sign In to Add a Plant</div>
+            <div>
+              <NewForm
+                isOpen={isOpen}
+                onClose={onClose}
+                setFirestorePlants={setFirestorePlants}
+                setDocumentIDs={setDocumentIDs}
+              />
             </div>
-          )}
-        </main>
-      </div>
-    </Layout>
+
+            {user ? (
+              <Button onClick={handleNewFormClick} className="mx-6 mb-10">
+                New Plant
+              </Button>
+            ) : (
+              <div className="mx-6 mb-10">
+                <div>Sign In to Add a Plant</div>
+              </div>
+            )}
+          </main>
+        </div>
+      </Layout>
+    </motion.div>
   );
 }
