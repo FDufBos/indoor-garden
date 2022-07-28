b82e58ed3821c55dacda49d51d70e80fe4c7cbcc
Francesco <fduffyboscagli@gmail.com>
2022-07-28 00:17:25 -0700

    :construction: Part of user discovery

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -176,6 +176,7 @@ export function UserAuthContextProvider({ children }) {
         setFirestorePlants,
         documentIDs,
         setDocumentIDs,
+        getthreeUserIDs,
       }}
     >
       {children}
