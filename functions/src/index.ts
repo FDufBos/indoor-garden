bcc6ec29f05238081846686aec1809d146b03186
Francesco <fduffyboscagli@gmail.com>
2022-07-30 12:18:11 -0700

    :tada: Firebase  functions init

---
+++ functions/src/index.ts
@@ -0,0 +1,14 @@
+// import * as functions from "firebase-functions";
+
+// const functions = require('firebase-functions');
+// const admin = require('firebase-admin');
+// admin.initializeApp();
+
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
