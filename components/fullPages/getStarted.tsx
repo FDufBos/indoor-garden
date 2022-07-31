e8c21a294c4e168d2f7aee8b448f07d83069f7f9
Francesco <fduffyboscagli@gmail.com>
2022-07-30 21:57:02 -0700

    :memo: added comments

--- components/fullPages/getStarted.tsx
+++ components/fullPages/getStarted.tsx
@@ -1,58 +1,63 @@
-import { Divider, Flex, Button, Icon, useToast } from "@chakra-ui/react";
-import { SignInButton, SignUpButton } from "../layout";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { FcGoogle } from "react-icons/fc";
+//Next imports
+import Head from "next/head";
 import { useRouter } from "next/router";
-import {useEffect } from "react";
-import { useUserAuth } from "../../contexts/AuthContext";
+
+//Firebase imports
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { doc, setDoc, serverTimestamp } from "firebase/firestore";
+import { useUserAuth } from "../../contexts/AuthContext";
 import { db } from "../../utils/firebaseUtils";
 import { addPlant } from "../../data/firestore";
 
+//UI  Imports
+import { Flex, Button, useToast } from "@chakra-ui/react";
+import { FcGoogle } from "react-icons/fc";
+import { SignInButton, SignUpButton } from "../layout";
 
-import Head from "next/head";
+//React import
+import { useEffect, useState } from "react";
 
+
+
+//Setup constants
 const auth = getAuth();
-
-import { useState } from "react";
 const provider = new GoogleAuthProvider();
 
-export default function GetStarted(props) {
-  const [showLoadingSpinner, setShowLoadingSpinner] = useState(false);
-  const {
-    user,
-    firestorePlants,
-    documentIDs,
-    setFirestorePlants,
-    setDocumentIDs,
-    logOut,
-    setName,
-    name
-  } = useUserAuth();
 
+//Default export
+export default function GetStarted() {
+
+  //Hooks
   const router = useRouter();
   const { success } = router.query;
   const toast = useToast();
-  const [email, setEmail] = useState("");
+  const { user, setName, name } = useUserAuth();
+  const [showLoadingSpinner, setShowLoadingSpinner] = useState(false);
 
+  //EFFECTS
+    //If URL "success" paramater is equal to "password-reset-email-sent"
+    //show success toast message
   useEffect(() => {
     if (success == "password-reset-email-sent") {
       toast({
         title: "Success",
-        position: 'top',
+        position: "top",
         description: "Check your email for a password reset link",
         status: "success",
         duration: 6000,
         isClosable: true,
         variant: "subtle",
         containerStyle: {
-          width: '95vw',
-          maxWidth: '900px',
+          width: "95vw",
+          maxWidth: "900px",
         },
       });
     }
   }, [success]);
 
+  //HANDLERS
+    //When user clicks "Sign in with Google" button
+    //log in with Google flow
   const handleGoogleClick = (e) => {
     e.preventDefault();
     signInWithPopup(auth, provider)
@@ -62,9 +67,10 @@ export default function GetStarted(props) {
         const token = credential.accessToken;
         // The signed-in user info.
         const googleUser = result.user;
-        
+
         // ...
-      }).then(async () => {
+      })
+      .then(async () => {
         await setDoc(doc(db, "users", auth.currentUser.uid), {
           uid: auth.currentUser.uid,
           authProvider: "Google",
@@ -109,7 +115,6 @@ export default function GetStarted(props) {
           Indoor Garden
         </h1>
       </div>
-      {/* <Divider size="lg" /> */}
       {/* TODO: make it so that this bottom part is attached to bottom isntead of being below the block above. */}
       <div className="flex flex-col items-center justify-center h-[50%]">
         <Flex
@@ -124,7 +129,12 @@ export default function GetStarted(props) {
             showLoadingSpinner={showLoadingSpinner}
             setShowLoadingSpinner={setShowLoadingSpinner}
           ></SignUpButton>
-          <Button onClick={handleGoogleClick} leftIcon={<FcGoogle />} size="sm" className="text-sm">
+          <Button
+            onClick={handleGoogleClick}
+            leftIcon={<FcGoogle />}
+            size="sm"
+            className="text-sm"
+          >
             Sign in with google
           </Button>
           <SignInButton
