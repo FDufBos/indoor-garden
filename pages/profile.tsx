7734d7b72028bb6c218e39b1a0b4dafb3dd9d851
Francesco <fduffyboscagli@gmail.com>
2022-10-10 18:25:07 -0700

    ♻️ Refactored profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,19 @@
+import Router, { useRouter } from "next/router";
+import { useUserAuth } from "../contexts/AuthContext";
+
+import ProfilePage from "../components/fullPages/profilePage";
+
+export default function User() {
+  //create state to store plant query param
+  const router = useRouter();
+  // const {userProfile} = router.query;
+  const { user, userDocument } = useUserAuth();
+
+
+
+  return (
+    <div>
+      <ProfilePage></ProfilePage>
+    </div>
+  );
+}
