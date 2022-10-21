b683096ee16a9ea7574f8a6cc930b626229911c2
Francesco <fduffyboscagli@gmail.com>
2022-10-20 21:24:25 -0700

    :art: Improved structure / format of the code.

--- pages/garden.tsx
+++ pages/garden.tsx
@@ -1,12 +1,12 @@
+import GetStarted from "@main/components/fullPages/getStarted";
+import Homepage from "@main/components/fullPages/homepage";
+import { useUserAuth } from "@main/contexts/AuthContext";
 import React from "react";
-
-import GetStarted from "../components/fullPages/getStarted";
-import Homepage from "../components/fullPages/homepage";
-import { useUserAuth } from "../contexts/AuthContext";
 
 export const Garden: React.FC = () => {
   const { user } = useUserAuth();
 
   return <div>{user ? <Homepage /> : <GetStarted />}</div>;
+
 };
 export default Garden;
