7027254102aafdb648dde7e340d0850d90841636
Francesco <fduffyboscagli@gmail.com>
2022-10-20 21:25:11 -0700

    :tada: Main codex page in the router

---
+++ pages/codex/index.tsx
@@ -0,0 +1,9 @@
+import SelectPlant from "@main/components/forms/add-plant/select-plant";
+
+export const CodexPage: React.FC = () => (
+  <div>
+    <SelectPlant/>
+  </div>
+)
+
+export default CodexPage
