377a87780e2b95dd13cb795abdbb547f59c5b189
Francesco <fduffyboscagli@gmail.com>
2022-10-21 17:16:14 -0700

    :lipstick: Making fonts more variable

--- styles/Fonts.tsx
+++ styles/Fonts.tsx
@@ -18,7 +18,9 @@ const Fonts: React.FC = () => (
       @font-face {
         font-family: 'Flexa';
         font-style: normal;
-        font-weight: 300 500;
+        font-weight: 50 950;
+        font-stretch: 75% 125%;
+        format("woff2-variations");
         font-display: swap;
         src: url('./assets/fonts/GT-Flexa-GX.woff2') format('woff2'),
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
