256965054477d8e1449a8eef3c326b1bb0731b26
Francesco <fduffyboscagli@gmail.com>
2022-10-21 17:16:55 -0700

    :construction: Under design and abstraction construction

--- components/fullPages/codexPage.tsx
+++ components/fullPages/codexPage.tsx
@@ -6,21 +6,23 @@ export const CodexPage: React.FC<Plant> = ({
   botanicalName,
   commonName,
   emoji,
-  id,
   soilType,
   toxicity,
 }) => (
-  <div>
-    <nav className="flex justify-between my-[18px] mx-[24px] items-center">
+  <div className="bg-monstera-100 min-h-screen">
+    <nav className="flex justify-between py-[18px] mx-[24px] items-center">
       <Link href="/codex" passHref>
-        <ChevronLeftIcon color="white" className="text-3xl cursor-pointer" />
+        <ChevronLeftIcon
+          color="slate.700"
+          className="text-4xl cursor-pointer"
+        />
       </Link>
-      <h1 className="font-[Flexa] font-medium text-[24px] -letter-spacing-[1%] text-center">
+      <h1 className="font-[Flexa] font-[400] text-[20px] -letter-spacing-[1%] text-center text-slate-700">
         {commonName[0]}
       </h1>
-      <div className="relative h-[36px] w-[36px] bg-white rounded-full">
-        <div className="absolute h-[24px] w-[24px] border-[2px] border-monstera-800 rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <div className="text-monstera-800 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[20px]">
+      <div className="relative h-[36px] w-[36px] bg-slate-700 rounded-full">
+        <div className="absolute h-[24px] w-[24px] border-[2px] border-white rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[20px]">
             +
           </div>
         </div>
@@ -37,32 +39,108 @@ export const CodexPage: React.FC<Plant> = ({
       </div>
     </section>
     <h2 className=" text-center relative -top-[64px] text-[80px]">{emoji}</h2>
+    <h3 className=" text-center relative -top-[80px] text-[14px] text-monstera-700 font-[400] opacity-[85%]">
+      {botanicalName}
+    </h3>
     <main className="relative -top-[56px]">
-      <div className="flex justify-between min-h-[72px] gap-[8px] mx-[24px] text-[#FFB800]">
-        <div className="w-full rounded-[16px] border border-[#FFB800] p-[8px] flex flex-col gap-[8px] justify-between">
-          <h3 className="font-bold text-[14px]">SOIL</h3>
-          <h3 className="h-full text-[16px] border border-[#FFB800] rounded-[12px] px-[8px] py-[4px] leading-4 tracking-tight">
+      <section className="flex justify-between min-h-[72px] gap-[8px] mx-[24px] text-monstera-700">
+        <div className="w-full rounded-[16px] border-monstera-700 bg-opacity-60 border shadow-sm p-[8px] flex flex-col gap-[8px] justify-between">
+          <h3 className="font-bold  text-[14px] pl-[4px]">SOIL</h3>
+          {/* eslint-disable-next-line max-len */}
+          <h3 className="flex border-monstera-700 items-center justify-start h-full text-[14px] leading-tighter border shadow-sm rounded-[12px] px-[8px] py-[4px] leading-4 tracking-tight">
             {soilType}
           </h3>
         </div>
-        <div className="w-full rounded-[16px] border border-[#FFB800] p-[8px] flex flex-col gap-[8px] justify-between">
-          <h3 className="font-bold text-[14px]">TOXICITY</h3>
-          <h3 className="h-full text-[16px] border border-[#FFB800] rounded-[12px] px-[8px] py-[6px] leading-4 tracking-tight">
+        <div className="w-full rounded-[16px] border-monstera-700  bg-opacity-60 border shadow-sm p-[8px] flex flex-col gap-[8px] justify-between">
+          <h3 className="font-bold text-[14px] pl-[4px]">TOXICITY</h3>
+          {/* eslint-disable-next-line max-len */}
+          <h3 className="flex border-monstera-700 items-center justify-start h-full text-[14px] leading-tighter border shadow-sm rounded-[12px] px-[8px] py-[6px] leading-4 tracking-tight">
             {toxicity || "Unknown"}
           </h3>
         </div>
-        <div className="w-full rounded-[16px] border border-[#FFB800] p-[8px] flex flex-col gap-[8px] justify-between">
-          <h3 className="font-bold text-[14px]">TYPE</h3>
-          <h3 className="h-full text-[16px] border border-[#FFB800] rounded-[12px] px-[8px] py-[6px] leading-4 tracking-tight">
-            😇
+        {/* eslint-disable-next-line max-len */}
+        <div className="w-full rounded-[16px] max-w-[64px] border-monstera-700 bg-opacity-60 border shadow-sm p-[8px] flex flex-col gap-[8px] justify-between">
+          <h3 className="font-bold text-[14px] leading-tighter pl-[4px]">
+            TYPE
           </h3>
+          {/* eslint-disable-next-line max-len */}
+          <div className="border-monstera-700 flex items-center justify-center h-full text-center text-xl border shadow-sm rounded-[12px] px-[8px] py-[6px] leading-4 tracking-tight">
+            <h3 className="relative top-[1px]">😇</h3>
+          </div>
         </div>
-      </div>
+      </section>
+      <section className="my-[32px]">
+        <div
+          id="water-slider"
+          className="relative h-[10px] border border-blue-300 mx-[24px] rounded-full z-50"
+        >
+          <div className="relative top-5">
+            <div className="absolute left-0 font-medium text-blue-400 text-[12px] opacity-75 translate-x-1/5">
+              1
+            </div>
+            <div className="absolute left-[75%] -translate-x-1/2 font-semibold text-blue-600">
+              7
+            </div>
+            <div className="absolute right-0 font-medium text-blue-400 text-[12px] opacity-75 -translate-x-1/5">
+              90+
+            </div>
+          </div>
+          <div className="z-40">
+            <div className="absolute bg-blue-400 left-1/4 w-[1px] h-[8px]" />
+            <div className="absolute bg-blue-400 left-2/4 w-[1px] h-[8px]" />
+            <div className="absolute bg-blue-400 left-3/4 w-[1px] h-[8px]" />
+          </div>
+          <div>
+            <div className="absolute -translate-y-1/2 top-1/2 left-0 w-[75%] h-2 bg-blue-200 rounded-full z-0" />
+            {/* eslint-disable-next-line max-len */}
+            <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-[75%] h-[30px] w-[30px] rounded-full border-slate-700 border-1 bg-white shadow-[1.21429px_1.21429px_12.1429px_rgba(0, 0, 0, 0.14);]">
+              <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 mt-[1px] left-1/2 text-[20px]">
+                💧
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          id="sunlight-slider"
+          className="relative h-[10px] border border-amber-300 mx-[24px] mt-[64px] rounded-full"
+        >
+          <div className="relative top-5">
+            <div className="absolute left-0 font-medium text-amber-500 text-[12px] opacity-75 translate-x-1/5">
+              DARK
+            </div>
+            <div className="absolute left-[55%] -translate-x-1/2 font-semibold text-amber-700">
+              Indirect
+            </div>
+            <div className="absolute right-0 font-medium text-amber-500 text-[12px] opacity-75 -translate-x-1/5">
+              DIRECT
+            </div>
+          </div>
+          <div>
+            <div className="absolute bg-amber-300 left-1/4 w-[1px] h-[8px]" />
+            <div className="absolute bg-amber-300 left-2/4 w-[1px] h-[8px]" />
+            <div className="absolute bg-amber-300 left-3/4 w-[1px] h-[8px]" />
+          </div>
+          <div>
+            <div className="absolute -translate-y-1/2 top-1/2 left-0 w-[55%] h-2 bg-amber-100 rounded-full" />
+            {/* eslint-disable-next-line max-len */}
+            <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-[55%] h-[30px] w-[30px] rounded-full bg-[#FCFEF8] shadow-[1.21429px_1.21429px_12.1429px_rgba(0, 0, 0, 0.14);]">
+              <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 mt-[1px] left-1/2 text-[20px]">
+                🌞
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="p-[24px] mt-[32px]">
+        <h3 className="text-monstera-700 font-[Alpina] text-[18px] font-bold">
+          Description
+        </h3>
+        <hr className="mt-[4px] border-monstera-700" />
+        <p className="text-monstera-700 mt-[8px] tracking-tight leading-[133.5%]">
+          {toxicity}
+        </p>
+      </section>
     </main>
-    <h1>{botanicalName}</h1>
-    <h2>{commonName}</h2>
-    <h2>{emoji}</h2>
-    <h2>{id}</h2>
   </div>
 );
 
