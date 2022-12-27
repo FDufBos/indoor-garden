0bc66eee1f60265bdf52c77897fb24b182982e50
Francesco <fduffyboscagli@gmail.com>
2022-12-27 10:24:19 -0800

    :rocket: updated to use  data from database 🚧 Need to finish this page so it has sun and water sliders working

--- components/fullPages/codexPage.tsx
+++ components/fullPages/codexPage.tsx
@@ -1,23 +1,31 @@
 import { ChevronLeftIcon } from "@chakra-ui/icons";
+import { Box } from "@chakra-ui/react";
 import { Plant } from "@main/common-types";
 import Link from "next/link";
 
 export const CodexPage: React.FC<Plant> = ({
   botanicalName,
   commonName,
-  emoji,
   soilType,
   toxicity,
+  sunExposure,
+  hardinessZones,
+  soilPh,
+  nativeAreas,
+  matureSize,
+  baseDaysBetweenWatering,
+  id,
+  bloomTime,
 }) => (
   <div className="bg-monstera-100 min-h-screen">
     <nav className="flex justify-between py-[18px] mx-[24px] items-center">
       <Link href="/codex" passHref>
         <ChevronLeftIcon
-          color="green.700"
-          className="text-4xl cursor-pointer"
+          color="green.900"
+          className="text-3xl cursor-pointer"
         />
       </Link>
-      <h1 className="font-[Flexa] font-[500] text-[20px] -letter-spacing-[1%] text-center text-monstera-700">
+      <h1 className="font-flexa font-[400] text-[22px] tracking-tight leading-[26px] text-center text-monstera-700">
         {commonName[0]}
       </h1>
       <div className="relative h-[36px] w-[36px] bg-monstera-700 rounded-full">
@@ -28,118 +36,156 @@ export const CodexPage: React.FC<Plant> = ({
         </div>
       </div>
     </nav>
-    <section
-      id="plant-photographs"
-      className="min-h-[215px] h-[32vw] flex w-full"
-    >
-      <div className="bg-[#bbe8ef] w-1/2" />
-      <div className="bg-[#f3d3e2] w-1/2 flex-col">
-        <div className="bg-[#dcf3ff] h-1/2" />
-        <div className="bg-[#f3d3e2]  h-1/2" />
+    <main>
+      <section
+        id="plant-photographs"
+        className="min-h-[215px] h-[32vw] flex w-full"
+      >
+        <div className="flex flex-col justify-center items-center w-full bg-sun-400 border border-y-black" />
+      </section>
+      <section className="grid grid-cols-5 grid-rows-[48px_36px_40px_36px_40px] gap-0 leading-[16px]">
+        <div className="flex justify-center items-center border-b border-black col-span-full font-alpina italic text-[16px] tracking-tight">
+          {botanicalName}
+        </div>
+        <div className="flex  border-r border-b border-black text-[18px] font-[500] italic font-alpina items-center pl-2">
+          Light
+        </div>
+        <div className="flex border-r border-b border-black text-[18px] font-[500] italic font-alpina items-center pl-2">
+          Water
+        </div>
+        <div className="flex col-span-3 border-b border-black text-[18px] font-[500] italic font-alpina items-center pl-2">
+          Soil
+        </div>
+        <div className="flex bg-yellow-300  col-span-1 row-span-3 border-b border-r border-black items-center pl-2" />
+        <div className="flex bg-[#5caebf] col-span-1 row-span-3 border-b border-r border-black items-center pl-2" />
+        <div className="flex bg-gray-300 col-span-3 border-b border-black items-center pl-2 text-[14px] font-[500]">
+          {soilType}
+        </div>
+        <div className="flex col-span-1 border-b border-r border-black text-[18px] font-[500] italic font-alpina items-center pl-2">
+          Zones
+        </div>
+        <div className="flex col-span-2 border-b border-black text-[18px] font-[500] italic items-center pl-2 font-alpina">
+          Mature Size
+        </div>
+        <div className="flex bg-gray-300 col-span-1 border-b border-r border-black items-center pl-2 text-[14px] font-[500]">
+          {hardinessZones}
+        </div>
+        <div className="flex bg-gray-300 col-span-2 border-b border-black items-center pl-2 text-[14px] font-[500]">
+          {matureSize}
+        </div>
+      </section>
+      <section className="relative -top-[8px] flex flex-col gap-[8px] m-[24px]">
+        <h2 className="relative top-[16px] font-alpina font-[500] text-[18px]">
+          Fact Sheet
+        </h2>
+        <div className="flex items-center w-full">
+          <div className="grow h-[1px] bg-black w-24" />
+          <div className=" text-xs pl-2 text-gray-600 font-alpina underline cursor-pointer">
+            Feedback
+          </div>
+        </div>
+        <h4 className="text-xs font-bold">
+          NATIVE AREA:{" "}
+          <span className="text-[16px] font-normal">
+            {nativeAreas.map((nativeArea) => (
+              <span key={nativeArea}>
+                {nativeArea}
+                {nativeAreas.indexOf(nativeArea) !== nativeAreas.length - 1
+                  ? ", "
+                  : ""}
+              </span>
+            ))}
+          </span>
+        </h4>
+        {commonName.length > 1 && (
+          <h4 className="text-xs font-bold">
+            COMMON NAMES:{" "}
+            <span className="text-[16px] font-normal">
+              {commonName.map((name) => (
+                <span key={name}>
+                  {name}
+                  {commonName.indexOf(name) !== commonName.length - 1
+                    ? ", "
+                    : ""}
+                </span>
+              ))}
+            </span>
+          </h4>
+        )}
+        {commonName.length === 1 && (
+          <h4 className="text-xs font-bold">
+            COMMON NAME:{" "}
+            <span className="text-[16px] font-normal">
+              {commonName.map((name) => (
+                <span key={name}>
+                  {name}
+                  {commonName.indexOf(name) !== commonName.length - 1
+                    ? ", "
+                    : ""}
+                </span>
+              ))}
+            </span>
+          </h4>
+        )}
+        <h4 className="text-xs font-bold">
+          BLOOM FREQUENCY:{" "}
+          <span className="text-[16px] font-normal">{bloomTime}</span>
+        </h4>
+        <h4 className="text-xs font-bold">
+          TOXICITY:{" "}
+          <span className="text-[16px] font-normal">
+            {toxicity || "No data available"}
+          </span>
+        </h4>
+        <h4 className="text-xs font-bold">
+          SOIL PH:{" "}
+          <span className="text-[16px] font-normal">
+            {soilPh || "No data available"}
+          </span>
+        </h4>
+      </section>
+      <section className="m-[24px]">
+        <h2 className="font-alpina font-[500] text-[18px]">Description</h2>
+        <div className="flex items-center w-full">
+          <div className="grow h-[1px] bg-black w-24" />
+        </div>
+      </section>
+      <div className="fixed right-[24px] bottom-safe">
+        <Link
+          href={{
+            pathname: "/garden/new",
+            query: { id },
+          }}
+          passHref
+        >
+          <Box
+            as="button"
+            lineHeight="1.2"
+            transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+            border="1px solid black"
+            px="16px"
+            py="11px"
+            borderRadius="999px"
+            fontSize="16px"
+            fontWeight="500"
+            bg="#FEE214"
+            color="#4b4f56"
+            boxShadow="1px 1px 0px #000000"
+            _hover={{ bg: "#FEE214" }}
+            _active={{
+              bg: "#FEE214",
+              transform: "scale(0.98)",
+              borderColor: "#bec3c9",
+            }}
+            _focus={{
+              boxShadow:
+                "0 0 1px 1px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)",
+            }}
+          >
+            Add to garden
+          </Box>
+        </Link>
       </div>
-    </section>
-    <h2 className=" text-center relative -top-[64px] text-[80px]">{emoji}</h2>
-    <h3 className=" text-center relative -top-[80px] text-[14px] text-monstera-700 font-[400] opacity-[85%]">
-      {botanicalName}
-    </h3>
-    <main className="relative -top-[56px]">
-      <section className="flex justify-between min-h-[72px] gap-[8px] mx-[24px] text-monstera-700">
-        <div className="w-full rounded-[16px] border-monstera-700 bg-opacity-60 border shadow-sm p-[8px] flex flex-col gap-[8px] justify-between">
-          <h3 className="font-bold  text-[14px] pl-[4px]">SOIL</h3>
-          {/* eslint-disable-next-line max-len */}
-          <h3 className="flex border-monstera-700 items-center justify-start h-full text-[14px] leading-tighter border shadow-sm rounded-[12px] px-[8px] py-[4px] leading-4 tracking-tight">
-            {soilType}
-          </h3>
-        </div>
-        <div className="w-full rounded-[16px] border-monstera-700  bg-opacity-60 border shadow-sm p-[8px] flex flex-col gap-[8px] justify-between">
-          <h3 className="font-bold text-[14px] pl-[4px]">TOXICITY</h3>
-          {/* eslint-disable-next-line max-len */}
-          <h3 className="flex border-monstera-700 items-center justify-start h-full text-[14px] leading-tighter border shadow-sm rounded-[12px] px-[8px] py-[6px] leading-4 tracking-tight">
-            {toxicity || "Unknown"}
-          </h3>
-        </div>
-        {/* eslint-disable-next-line max-len */}
-        <div className="w-full rounded-[16px] max-w-[64px] border-monstera-700 bg-opacity-60 border shadow-sm p-[8px] flex flex-col gap-[8px] justify-between">
-          <h3 className="font-bold text-[14px] leading-tighter pl-[4px]">
-            TYPE
-          </h3>
-          {/* eslint-disable-next-line max-len */}
-          <div className="border-monstera-700 flex items-center justify-center h-full text-center text-xl border shadow-sm rounded-[12px] px-[8px] py-[6px] leading-4 tracking-tight">
-            <h3 className="relative top-[1px]">😇</h3>
-          </div>
-        </div>
-      </section>
-      <section className="my-[32px]">
-        <div
-          id="water-slider"
-          className="relative h-[10px] border border-blue-300 mx-[24px] rounded-full z-50"
-        >
-          <div className="relative top-5">
-            <div className="absolute left-0 font-medium text-blue-400 text-[12px] opacity-75 translate-x-1/5">
-              1
-            </div>
-            <div className="absolute left-[75%] -translate-x-1/2 font-semibold text-blue-600">
-              7
-            </div>
-            <div className="absolute right-0 font-medium text-blue-400 text-[12px] opacity-75 -translate-x-1/5">
-              90+
-            </div>
-          </div>
-          <div className="z-40">
-            <div className="absolute bg-blue-400 left-1/4 w-[1px] h-[8px]" />
-            <div className="absolute bg-blue-400 left-2/4 w-[1px] h-[8px]" />
-            <div className="absolute bg-blue-400 left-3/4 w-[1px] h-[8px]" />
-          </div>
-          <div>
-            <div className="absolute -translate-y-1/2 top-1/2 left-0 w-[75%] h-2 bg-blue-200 rounded-full z-0" />
-            {/* eslint-disable-next-line max-len */}
-            <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-[75%] h-[30px] w-[30px] rounded-full border-slate-700 border-1 bg-white shadow-[1.21429px_1.21429px_12.1429px_rgba(0, 0, 0, 0.14);]">
-              <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 mt-[1px] left-1/2 text-[20px]">
-                💧
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          id="sunlight-slider"
-          className="relative h-[10px] border border-amber-300 mx-[24px] mt-[64px] rounded-full"
-        >
-          <div className="relative top-5">
-            <div className="absolute left-0 font-medium text-amber-500 text-[12px] opacity-75 translate-x-1/5">
-              DARK
-            </div>
-            <div className="absolute left-[55%] -translate-x-1/2 font-semibold text-amber-700">
-              Indirect
-            </div>
-            <div className="absolute right-0 font-medium text-amber-500 text-[12px] opacity-75 -translate-x-1/5">
-              DIRECT
-            </div>
-          </div>
-          <div>
-            <div className="absolute bg-amber-300 left-1/4 w-[1px] h-[8px]" />
-            <div className="absolute bg-amber-300 left-2/4 w-[1px] h-[8px]" />
-            <div className="absolute bg-amber-300 left-3/4 w-[1px] h-[8px]" />
-          </div>
-          <div>
-            <div className="absolute -translate-y-1/2 top-1/2 left-0 w-[55%] h-2 bg-amber-100 rounded-full" />
-            {/* eslint-disable-next-line max-len */}
-            <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-[55%] h-[30px] w-[30px] rounded-full bg-[#FCFEF8] shadow-[1.21429px_1.21429px_12.1429px_rgba(0, 0, 0, 0.14);]">
-              <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 mt-[1px] left-1/2 text-[20px]">
-                🌞
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="p-[24px] mt-[32px]">
-        <h3 className="text-monstera-700 font-[Alpina] text-[18px] font-bold">
-          Description
-        </h3>
-        <hr className="mt-[4px] border-monstera-700" />
-        <p className="text-monstera-700 mt-[8px] tracking-tight leading-[133.5%]">
-          {toxicity}
-        </p>
-      </section>
     </main>
   </div>
 );
