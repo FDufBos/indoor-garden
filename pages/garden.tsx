490abd718b7e5874551bcfaa7384943f3a377821
Francesco <fduffyboscagli@gmail.com>
2022-12-27 13:31:15 -0800

    :wastebasket: deleted a bunch of un-necessary code now that auth occurs on _app.tsx

--- pages/garden.tsx
+++ pages/garden.tsx
@@ -1,17 +1,5 @@
-import {Spinner} from "@chakra-ui/react";
-import GetStarted from "@main/components/fullPages/getStarted";
 import Homepage from "@main/components/fullPages/homepage";
-import { useUserAuth } from "@main/contexts/AuthContext";
 import React from "react";
 
-export const Garden: React.FC = () => {
-  const { user } = useUserAuth();
-
-  if (user === undefined) {
-    return <Spinner />;
-  }
-
-  return <div>{user ? <Homepage /> : <GetStarted />}</div>;
-
-};
+export const Garden: React.FC = () => <Homepage />;
 export default Garden;
