134416b427b72c1e75ca755b38dbb48767b1abab
Francesco <fduffyboscagli@gmail.com>
2023-03-04 23:42:16 -0800

    🗑️ removed unused code

--- utils/firebaseUtils.ts
+++ utils/firebaseUtils.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { CACHE_SIZE_UNLIMITED, getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Initialize Firebase
