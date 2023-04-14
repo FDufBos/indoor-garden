c8b0610f5a4ffe689f82cc95723aff0fa538d4cd
Francesco <fduffyboscagli@gmail.com>
2023-04-14 15:28:09 -0700

    Catch errors on getbasedaysbetweenwatering

--- components/fullPages/homepage.tsx
+++ components/fullPages/homepage.tsx
@@ -51,8 +51,12 @@ export const Homepage: React.FC = ({  }) => {
   } = useFirestoreQuery<Plant>("plants");
   // return the base days between watering from the codex for each plant
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  const getBaseDaysBetweenWatering = (plant: Plant) =>
-    plant.baseDaysBetweenWatering;
+  const getBaseDaysBetweenWatering = (plant: Plant) => {
+    if (!plant) {
+      return 0; // or another default value
+    }
+    return plant.baseDaysBetweenWatering;
+  };
     
 
   // Framer Animation Variants
